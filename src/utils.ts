7ccb05d1e2e8b65e03b9d14e74e7e80a002c68c6
Nnisarg Gada <gadannisarg@gmail.com>
2025-03-05 11:53:42 +0530

    init

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,23 @@
 export const metaData = {
-  title: "Launchpad | A minimal Astro Starter by Nnisarg Gada",
-  description: "Launchpad is a fast, minimal, and SEO-friendly Astro starter template designed for modern web projects.",
-  image: "https://launchpad.nnisarg.in/poster.jpg",
-  author: "Nnisarg Gada",
+  title: "Mumbai GNU/Linux Users' Group",
+  description: "A community-driven GNU/Linux Users Group (GLUG) in Mumbai promoting Swatantra Software, open-source collaboration, and digital freedom through advocacy, education, and user support.",
+  image: "https://mumbaiglug.org/poster.jpg",
+  author: "Mumbai GLUG",
 };
 
 export const umami = {
   src: "https://umami.nnisarg.in/script.js",
-  id: "e318ec0d-e7ca-4b97-8517-fbfe3e40e56d",
-  domains: "launchpad.nnisarg.in",
+  id: "website-id",
+  domains: "mumbaiglug.org, www.mumbaiglug.org",
 };
+
+export const navLinks = [
+  {
+    name: "Join",
+    href: "/#join",
+  },
+  {
+    name: "Contact",
+    href: "/#join",
+  }
+]
