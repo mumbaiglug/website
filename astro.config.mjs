22034720399cee6e6dc208a1d0d6038b252e206f
Nnisarg Gada <contact@nnisarg.in>
2025-03-22 04:32:25 +0530

    nnisarggada -> hect1k

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://launchpad.nnisarg.in",
+  site: "https://mumbaiglug.org",
 
   prefetch: {
     prefetchAll: true,
